8125b34108e4cd9d5ae317c35284819ccdd13ac6
Nicolas Chambrier <naholyr@gmail.com>
2015-06-03 17:37:51 +0200

    Throttle calls to /session when typing

--- shared/components/app.js
+++ shared/components/app.js
@@ -5,15 +5,19 @@ import StatusMessage from "./status-message"
 import PollTable from "./poll-table"
 import PollReport from "./poll-report"
 import Nav from "./nav"
+import throttle from "lodash/function/throttle"
 
+
+// Declare here for global throttling
+const updateWho = throttle((flux, who) => flux.getActions("poll").updateWho(who), 5000)
 
 export default class App extends React.Component {
-  onChangeWho(who) {
-    this.props.flux.getActions("poll").updateWho(who)
+  onChangeWho(e) {
+    updateWho(this.props.flux, e.target.value)
   }
 
   render() {
-    const input = this.props.showTable ? <input placeholder="Entrez votre nom" value={ this.props.who } onChange={ (e) => this.onChangeWho(e.target.value) } /> : null
+    const input = this.props.showTable ? <input placeholder="Entrez votre nom" defaultValue={ this.props.who } onChange={ e => this.onChangeWho(e) } /> : null
     const table = this.props.showTable ? <PollTable { ...this.props } /> : null
     const report = this.props.showReport ? <PollReport { ...this.props } /> : null
 
