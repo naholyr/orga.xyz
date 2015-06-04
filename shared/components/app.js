211c8d3cdc1daf82917d1fbf0a5a20d9fa3b68b6
Nicolas Chambrier <naholyr@gmail.com>
2015-06-04 22:56:41 +0200

    Fix issue with 'who' lagging behind user typing

--- shared/components/app.js
+++ shared/components/app.js
@@ -12,20 +12,31 @@ import throttle from "lodash/function/throttle"
 const updateWho = throttle((flux, who) => flux.getActions("poll").updateWho(who), 5000)
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    // "who" is a state here for better reactivity (*)
+    this.state = {"who": props.who}
+  }
+
   onChangeWho(e) {
+    // (*) Note this function is throttled because it's an onChange
+    // (*) so prop "who" will not always be updated, which could cause undecent latency
     updateWho(this.props.flux, e.target.value)
+    // (*) So we rely on state
+    this.setState({"who": e.target.value})
   }
 
   render() {
-    const input = this.props.showTable ? <input placeholder="Entrez votre nom" defaultValue={ this.props.who } onChange={ e => this.onChangeWho(e) } /> : null
-    const table = this.props.showTable ? <PollTable { ...this.props } /> : null
-    const report = this.props.showReport ? <PollReport { ...this.props } /> : null
+    const props = {...this.props, ...this.state}
+    const input = this.props.showTable ? <input placeholder="Entrez votre nom" defaultValue={ this.state.who } onChange={ e => this.onChangeWho(e) } /> : null
+    const table = this.props.showTable ? <PollTable { ...props } /> : null
+    const report = this.props.showReport ? <PollReport { ...props } /> : null
 
     return (
       <div className="poll">
         { input }
         { table }
-        <Nav { ...this.props } />
+        <Nav { ...props } />
         { report }
         <StatusMessage />
       </div>
