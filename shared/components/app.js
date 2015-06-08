cab3db8cf5763239f203fc29541fde81dec47000
Nicolas Chambrier <naholyr@gmail.com>
2015-06-09 00:42:12 +0200

    FIX missing 'who' prop

--- shared/components/app.js
+++ shared/components/app.js
@@ -28,6 +28,10 @@ class App extends React.Component {
     "showReport": React.PropTypes.bool.isRequired
   }
 
+  static defaultProps = {
+    "who": ""
+  }
+
   constructor(props) {
     super(props)
 
